7e83252f9586f495b7d06079c5cc4f01ed56fccc
moksh21102014 <104289232+moksh21102014@users.noreply.github.com>
2022-06-18 22:01:59 +0530

    Add files via upload

---
+++ Assignment-5 Output.py
@@ -0,0 +1,101 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/Moksh/OneDrive/Desktop/Assignment 5 input Q.py =====
+ANS.1
+Enter the the string :ELEPHANT
+TNAHPELE
+ANS.2
+Enter the minimum value :2
+Enter the maximum value: 80
+Enter the number for divisibility check :8
+8
+16
+24
+32
+40
+48
+56
+64
+72
+80
+ANS.3
+Enter the length of side:5
+Enter the length of side:8
+Enter the length of side:9
+19.8997487421324
+ANS.4
+Enter the no. of rows :9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+ANS.5
+Enter the number of rows5
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+ANS.6
+Enter the Lowest Range Value : 1
+Enter the Upper Range Value : 86
+The Prime Numbers in this range are : 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+ANS.7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+ANS.8
+Enter the number 98
+Enter the number -88
+Enter the number -65
+Enter the number -43
+Enter the number 91
+Enter the number 73
+Enter the number 28
+Enter the number 64
+Enter the number 98
+Enter the number 91
+Positive numbers are:  [98, 91, 73, 28, 64, 98, 91]
+Negative numbers are:  [-88, -65, -43]
+Even numbers are:  [98, -88, 28, 64, 98]
+Odd numbers are:  [-65, -43, 91, 73, 91]
+Number of times each number occurs in the List {98: 2, -88: 1, -65: 1, -43: 1, 91: 2, 73: 1, 28: 1, 64: 1}
+ANS.9
+Number of words: 8
+Enter the word: SKY
+Enter the word: SLEEP
+Enter the word: OPEN
+Enter the word: CLOSE
+Enter the word: OPEN
+Enter the word: SLICE
+Enter the word: LIVE
+Enter the word: SLICE
+Number of occurences:  {'SKY': 1, 'SLEEP': 1, 'OPEN': 2, 'CLOSE': 1, 'SLICE': 2, 'LIVE': 1}
